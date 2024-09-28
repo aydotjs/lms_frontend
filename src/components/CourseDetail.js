9d7bca4fa49e1cdd6db5e7a6108c6383261cb080
aydotjs <ayobami66@gmail.com>
2024-09-28 05:16:15 +0100

    video content

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -34,19 +34,48 @@ function CourseDetail() {
             Introduction
             <span className="float-end">
               <span className="me-5">1 Hour 30 Minutes</span>
-              <button className="btn btn-sm btn-danger">
+              <button
+                className="btn btn-sm btn-danger"
+                data-bs-toggle="modal"
+                data-bs-target="#videoModal1"
+              >
                 <i className="bi-youtube"></i>
               </button>
             </span>
-          </li>
-          <li className="list-group-item">
-            Introduction
-            <span className="float-end">
-              <span className="me-5">1 Hour 30 Minutes</span>
-              <button className="btn btn-sm btn-danger">
-                <i className="bi-youtube"></i>
-              </button>
-            </span>
+            {/* Video Modal Start */}
+            <div
+              className="modal fade"
+              id="videoModal1"
+              tabIndex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-lg">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      Video 1
+                    </h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <div class="ratio ratio-16x9">
+                      <iframe
+                        src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                        title="YouTube video"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Video Modal End */}
           </li>
           <li className="list-group-item">
             Introduction
@@ -58,8 +87,15 @@ function CourseDetail() {
             </span>
           </li>
 
-       
-          
+          <li className="list-group-item">
+            Introduction
+            <span className="float-end">
+              <span className="me-5">1 Hour 30 Minutes</span>
+              <button className="btn btn-sm btn-danger">
+                <i className="bi-youtube"></i>
+              </button>
+            </span>
+          </li>
         </ul>
       </div>
       <h5 className="pb-1 mb-4 mt-5">Related Courses </h5>
