87ba359937a3173799f1cede945a9cfb0d87e16f
aydotjs <ayobami66@gmail.com>
2024-09-29 23:42:53 +0100

    added teacher dashboard  page title

--- src/components/Teacher/TeacherDashBoard.js
+++ src/components/Teacher/TeacherDashBoard.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import TeacherSidebar from "./TeacherSidebar";
-
+import { useEffect } from "react";
 function TeacherDashboard() {
+  document.title = "Language4All-Teacher Dashboard";
   return (
     <div className="container mt-4">
       <div className="row">
