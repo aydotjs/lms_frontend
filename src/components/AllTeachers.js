3cb5064324eaa2022a746afb5b180527cccf4862
aydotjs <ayobami66@gmail.com>
2024-09-30 15:08:28 +0100

    fetched all tachers

--- src/components/AllTeachers.js
+++ src/components/AllTeachers.js
@@ -1,78 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+
+const baseUrl = "http://127.0.0.1:8000/api";
+
 export default function AllTeachers() {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/teacher`)
+      .then((response) => {
+        setTeachers(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the teachers!", error);
+      });
+  }, []);
+
   return (
     <div className="container mt-5">
-      <h3 className="pb-1 mb-4 mt-5">All Teachers</h3>
+      <h3 className="pb-1 mb-4 mt-5">All Our Teachers</h3>
       <div className="row mb-4">
-        <div className="col-md-3">
-          <div className="card">
-            <Link to="/teacher-detail/1">
-              <img src="logo512.png" className="card-img-top" alt="..." />
-            </Link>
-            <div className="card-body">
-              <h5 className="card-title">
-                <Link to="/teacher-detail/1">Teacher Name</Link>
-              </h5>
-            </div>
-            <div className="card-footer">
-              <div className="title">
-                <span>Rating: 4.5/5</span>
+        {teachers && teachers.length > 0 ? (
+          teachers.map((teacher, index) => (
+            <div className="col-md-3" key={index}>
+              <div className="card">
+                <Link to={`/teacher-detail/${teacher.id}`}>
+                  <img src="logo512.png" className="card-img-top" alt={teacher.name} />
+                </Link>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <Link to={`/teacher-detail/${teacher.id}`}>{teacher.name}</Link>
+                  </h5>
+                </div>
+                <div className="card-footer">
+                  <div className="title">
+                    <span>Rating: {teacher.rating || 'N/A'}/5</span>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <Link to="/teacher-detail/1">
-              <img src="logo512.png" className="card-img-top" alt="..." />
-            </Link>
-            <div className="card-body">
-              <h5 className="card-title">
-                <Link to="/teacher-detail/1">Teacher Name</Link>
-              </h5>
-            </div>
-            <div className="card-footer">
-              <div className="title">
-                <span>Rating: 4.5/5</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <Link to="/teacher-detail/1">
-              <img src="logo512.png" className="card-img-top" alt="..." />
-            </Link>
-            <div className="card-body">
-              <h5 className="card-title">
-                <Link to="/teacher-detail/1">Teacher Name</Link>
-              </h5>
-            </div>
-            <div className="card-footer">
-              <div className="title">
-                <span>Rating: 4.5/5</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <Link to="/teacher-detail/1">
-              <img src="logo512.png" className="card-img-top" alt="..." />
-            </Link>
-            <div className="card-body">
-              <h5 className="card-title">
-                <Link to="/teacher-detail/1">Teacher Name</Link>
-              </h5>
-            </div>
-            <div className="card-footer">
-              <div className="title">
-                <span>Rating: 4.5/5</span>
-              </div>
-            </div>
-          </div>
-        </div>
+          ))
+        ) : (
+          <p>No teachers available at the moment.</p>
+        )}
       </div>
 
       {/* Pagination Start */}
