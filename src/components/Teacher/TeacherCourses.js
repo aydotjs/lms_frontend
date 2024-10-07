f434c6802d614845d7566bc84ce978a8602ba2a9
aydotjs <ayobami66@gmail.com>
2024-10-07 18:19:21 +0100

    modified query set

--- src/components/Teacher/TeacherCourses.js
+++ src/components/Teacher/TeacherCourses.js
@@ -2,49 +2,63 @@ import React from "react";
 import { Link } from "react-router-dom";
 import TeacherSidebar from "./TeacherSidebar";
 import { useState, useEffect } from "react";
-const baseUrl = "http://127.0.0.1:8000/api";
 import axios from "axios";
+const baseUrl = "http://127.0.0.1:8000/api";
+
 export default function TeacherCourses() {
-  const [courseData, setCourseData] = useState([])
-  useEffect(()=>{
-    axios.get()
-  })
+  const [courseData, setCourseData] = useState([]);
+  // fetch courses when page load
+  useEffect(() => {
+    try {
+      axios.get(baseUrl + "/course").then((res) => {
+        setCourseData(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  console.log(courseData);
   return (
     <div className="container mt-4">
       <div className="row">
         <aside className="col-md-3">
           <TeacherSidebar />
         </aside>
-    <section className="col-md-9">
-    <div className="card">
-      <h5 className="card-header">My Courses</h5>
-      <div className="card-body">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Total Enrolled</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>English Language</td>
-              <td>
-                <Link to="/">10</Link>
-              </td>
-              <td>
-                <button className="btn btn-primary btn-danger active">
-                  Delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <section className="col-md-9">
+          <div className="card">
+            <h5 className="card-header">My Courses</h5>
+            <div className="card-body">
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Total Enrolled</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {courseData.map((course, index) => {
+                    return (
+                      <tr>
+                        <td>{course.title}</td>
+                        <td>
+                          <Link to="/">10</Link>
+                        </td>
+                        <td>
+                          <button className="btn btn-primary btn-danger">
+                            Delete
+                          </button>
+                          <Link className="btn btn-success btn-sm ms-2" to="/add-chapter/2">Add Chapter</Link>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
-    </section>
-    </div>
     </div>
   );
 }
