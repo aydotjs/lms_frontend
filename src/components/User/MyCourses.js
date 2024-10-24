7175780ab38971ff7cc6cb60e106fa4d03db7080
aydotjs <ayobami66@gmail.com>
2024-10-24 22:31:53 +0100

    fentching all courses for student

--- src/components/User/MyCourses.js
+++ src/components/User/MyCourses.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "./Sidebar";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+const baseUrl = "http://127.0.0.1:8000/api"
 export default function MyCourses() {
+  const [courseData, setCourseData] = useState([]);
+  const studentId =  localStorage.getItem("studentId")
+    // fetch courses when page load
+    useEffect(() => {
+      try {
+        axios.get(baseUrl + "/fetch-enrolled-courses/" + studentId).then((res) => {
+          setStudentData(res.data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+  
   return (
     <div className="container mt-4">
       <div className="row">
