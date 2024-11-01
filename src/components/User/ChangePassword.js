4b3774363a50cb5d701240aa7004b3fc5a5d44c5
aydotjs <ayobami66@gmail.com>
2024-11-01 23:03:39 +0100

    teacher change password

--- src/components/User/ChangePassword.js
+++ src/components/User/ChangePassword.js
@@ -43,8 +43,8 @@ function ChangePassword() {
     }, []);
 
     const studentLoginStatus = localStorage.getItem('teacherLoginStatus');
-    if (teacherLoginStatus !== 'true') {
-        window.location.href = '/teacher-login';
+    if (studentLoginStatus !== 'true') {
+        window.location.href = '/student-login';
     }
 
     return (
@@ -60,7 +60,7 @@ function ChangePassword() {
                             <div className="mb-3 row">
                                 <label htmlFor="inputPassword" className="col-sm-2 col-form-label">New Password</label>
                                 <div className="col-sm-10">
-                                    <input type="text" name="password" value={teacherData.password} onChange={handleChange} className="form-control" id="inputPassword" />
+                                    <input type="text" name="password" value={studentData.password} onChange={handleChange} className="form-control" id="inputPassword" />
                                 </div>
                             </div>
                             <hr />
