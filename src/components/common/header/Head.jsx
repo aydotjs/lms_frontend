a198ba1b46d47682ce7a7dfc3f9807ce029376a6
aydotjs <ayobami66@gmail.com>
2024-11-24 13:26:54 +0100

    hello

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,25 +1,35 @@
-import React from "react"
-
+import React from "react";
+import "./Head.css";
 const Head = () => {
   return (
     <>
-      <section className='head'>
-        <div className='container flexSB'>
-          <div className='logo'>
-            <h1>AMBESTEN ACADEMY</h1>
+      <section className="head">
+        <div className="container flexSB">
+          <div className="logo">
+            <h1 style={{ textAlign: "center" }}>
+              AMBESTEN
+              <br />
+              <span className="academy">
+                <span className="lines"></span>
+                <span className="lines"></span>
+                ACADEMY
+                <span className="lines"></span>
+                <span className="lines"></span>
+              </span>
+            </h1>
             <span>ONLINE TUTOR FOR ALL LANGUAGES</span>
           </div>
 
-          <div className='social'>
-            <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-instagram icon'></i>
-            <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-youtube icon'></i>
+          <div className="social">
+            <i className="fab fa-facebook-f icon"></i>
+            <i className="fab fa-instagram icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-youtube icon"></i>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
