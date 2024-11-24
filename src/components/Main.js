612c2516ad167edcb3147b7511472611e9d0c29c
aydotjs <ayobami66@gmail.com>
2024-11-24 09:10:06 +0100

    landing page  overhaul

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import Home from "./Home";
-import Header from "./Header";
-import Footer from "./Footer";
+import Home from "./home/Home.jsx";
+import Header from "./common/header/Header.jsx";
+import Footer from "./common/footer/Footer.jsx";
 import { Route, Routes } from "react-router-dom"; // Avoid aliasing Routes as Switch
 import AboutUsPage from "./AboutUsPage";
 import CourseDetail from "./CourseDetail";
@@ -40,7 +40,7 @@ import StudentAssignment from "./User/StudentAssignment.js";
 import MyTeachers from "./User/MyTeachers.js";
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -85,7 +85,7 @@ function App() {
         <Route path="/my-teachers" element={<MyTeachers/>} />
       </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO AMBESTEN' title='Best Online Education Expertise' />
+            <p>Make language learning a habit. Set aside time each day for practice. 5 minutes daily could become a lot</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
